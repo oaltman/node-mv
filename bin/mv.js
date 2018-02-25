c9ed98a68dc86bd806ac0413ad4e2ed7b1bed924
Ondřej Altman <ondrej.altman@socialbakers.com>
2018-02-25 17:22:19 +0100

    ES6 rewrite 1/2

--- bin/mv.js
+++ bin/mv.js
@@ -1,48 +1,35 @@
 #!/usr/bin/env node
-
-/**
- * Module dependencies.
- */
-
-var path = require('path'),
-  util = require('util'),
-  program = require('commander'),
-  mv = require('../index.js');
-
+// @ts-check
+const path = require('path')
+const program = require('commander')
+const mv = require('../index.js')
 
 function regexList(val) {
-  var list = val.split(','), i = 0, len = list.length;
-  var ret = [];
-  for (; i<len; i++) {
-    ret.push(new RegExp(list[i], "g"));
-  }
-  return ret;
+	return val
+		.split(',')
+		.map(x => new RegExp(x, "g"))
 }
 
 program
-  .version('0.0.1')
-  // Not implemented yet
-  .option('-g, --git', 'Rename in git')
-  .option('-e, --excludes <items>', 'List of regex for dir/files to excludes', regexList)
-  .parse(process.argv);
+	.version('0.0.1')
+	.option('-g, --git', 'Rename in git')
+	.option('-e, --excludes <items>', 'List of regex for dir/files to excludes', regexList)
+	.parse(process.argv);
 
 if (process.argv.length < 4) {
-  console.log(program.help());
+	console.log(program.help());
 }
 
-var source = process.argv[2],
-  dest = process.argv[3];
-
-var currentDir = process.cwd(),
-  originalPath = path.join(currentDir, source),
-  destAbsPath = path.join(currentDir, dest);
-
+const source = process.argv[2]
+const dest = process.argv[3]
+const currentDir = process.cwd()
+const	originalPath = path.join(currentDir, source)
+const	destAbsPath = path.join(currentDir, dest)
 
 mv(currentDir, originalPath, destAbsPath, program, function(err) {
-  if (err) {
-    console.error(util.inspect(err, {depth: 10}));
-    process.exit(1);
-  }
-
-  process.exit();
+	if (err) {
+		console.error(err);
+		process.exit(1);
+	}
+	process.exit();
 })
--- index.js
+++ index.js
@@ -1,173 +1,173 @@
-var path = require('path'),
-  fs = require('fs'),
-  exec = require('child_process').exec,
-  async = require('async'),
-  ProgressBar = require('progress'),
-  walk = require('./lib/walk');
-
+const path = require('path')
+const fs = require('fs')
+const exec = require('child_process').exec
+const async = require('async')
+const ProgressBar = require('progress')
+const walk = require('./lib/walk')
 var defaultRegExcludes = [/^\.+.*/, /node_modules/];
-
 var bar = null;
 
-module.exports = exports = function(currentDir, originalPath, newPath, options, cb) {
-  fs.exists(originalPath, function(exists){
-    if (!exists) return cb(new Error(originalPath + ' does not exist!'));
+function main(currentDir, originalPath, newPath, options, cb) {
+	fs.exists(originalPath, function(exists){
+		if (!exists) return cb(new Error(originalPath + ' does not exist!'));
 
-    fs.stat(originalPath, function (err, stat) {
-      if (err) return cb(err);
+		fs.stat(originalPath, function (err, stat) {
+			if (err) return cb(err);
 
-      if (stat.isDirectory()) {
-        mvDir(currentDir, originalPath, newPath, options, cb)
-      } else {
-        mvFile(currentDir, originalPath, newPath, options, cb)
-      }
-    });
-  })
+			if (stat.isDirectory()) {
+				mvDir(currentDir, originalPath, newPath, options, cb)
+			} else {
+				mvFile(currentDir, originalPath, newPath, options, cb)
+			}
+		});
+	})
 };
 
 function mvFile(currentDir, originalFilePath, newFilePath, options, cb) {
-  fs.exists(newFilePath, function(exists) {
-    steps = [function(cb) {
-      rename(originalFilePath, newFilePath, options.git, cb)
-    }];
-    bar = new ProgressBar(':bar', {total: 1});
-    excludes = getExcludes(options);
-    steps.push(function(cb) {updateReferencesInMovedFile(originalFilePath, newFilePath, null, cb)});
-    steps.push(function(cb) {updateReferencesToMovedFile(currentDir, originalFilePath, newFilePath, excludes, cb)});
+	fs.exists(newFilePath, function(exists) {
+		const steps = [function(cb) {
+			rename(originalFilePath, newFilePath, options.git, cb)
+		}];
+		const excludes = getExcludes(options);
+		steps.push(function(cb) {updateReferencesInMovedFile(originalFilePath, newFilePath, null, cb)});
+		steps.push(function(cb) {updateReferencesToMovedFile(currentDir, originalFilePath, newFilePath, excludes, cb)});
 
-    async.series(steps, cb);
-  })
+		async.series(steps, cb);
+	})
 }
 
 function getExcludes(options) {
-  var excludes = defaultRegExcludes, steps = [];
-  if (options.excludes) {
-    excludes = excludes.concat(options.excludes);
-  }
-  return excludes;
+	var excludes = defaultRegExcludes, steps = [];
+	if (options.excludes) {
+		excludes = excludes.concat(options.excludes);
+	}
+	return excludes;
 }
 
 function mvDir(currentDir, originalDirPath, newDirPath, options, cb) {
-  originalDirPath = originalDirPath.replace(/\/$/, '');
+	originalDirPath = originalDirPath.replace(/\/$/, '');
 
-  console.log('newDirPath', newDirPath);
-  if (newDirPath[newDirPath.length - 1] === '/') {
-    var pathParts = originalDirPath.split(path.sep);
-    newDirPath = newDirPath + pathParts[pathParts.length - 1];
-  }
+	console.log('newDirPath', newDirPath);
+	if (newDirPath[newDirPath.length - 1] === '/') {
+		var pathParts = originalDirPath.split(path.sep);
+		newDirPath = newDirPath + pathParts[pathParts.length - 1];
+	}
 
-  fs.exists(newDirPath, function(exists) {
-    if (exists) return cb(new Error(newDirPath + ' is already exist!'));
+	fs.exists(newDirPath, function(exists) {
+		if (exists) return cb(new Error(newDirPath + ' is already exist!'));
 
-    walk(originalDirPath, [], function(err, files) {
-      if (err) return cb(err);
+		walk(originalDirPath, [], function(err, files) {
+			if (err) return cb(err);
 
-      bar = new ProgressBar('  processing [:bar] :percent :etas', {total: files.length});
+			bar = new ProgressBar('  processing [:bar] :percent :etas', {total: files.length});
 
-      rename(originalDirPath, newDirPath, options.git, function(err) {
-        if (err) return cb(err);
+			rename(originalDirPath, newDirPath, options.git, function(err) {
+				if (err) return cb(err);
 
-        async.eachSeries(files, function(file, cb) {
-          bar.tick();
-          var newFilePath = file.replace(originalDirPath, newDirPath);
+				async.eachSeries(files, function(file, cb) {
+					bar.tick();
+					var newFilePath = file.replace(originalDirPath, newDirPath);
 
-          steps = [];
-          steps.push(function(cb) {
-            var excludeRequires = '^' + originalDirPath;
-            updateReferencesInMovedFile(file, newFilePath, new RegExp(excludeRequires), cb)
-          });
-          searchExcludes = getExcludes(options);
-          steps.push(function(cb) {
-            updateReferencesToMovedFile(currentDir, file, newFilePath, searchExcludes, cb)
-          });
+					const steps = [];
+					steps.push(function(cb) {
+						var excludeRequires = '^' + originalDirPath;
+						updateReferencesInMovedFile(file, newFilePath, new RegExp(excludeRequires), cb)
+					});
+					const searchExcludes = getExcludes(options);
+					steps.push(function(cb) {
+						updateReferencesToMovedFile(currentDir, file, newFilePath, searchExcludes, cb)
+					});
 
-          async.parallel(steps, cb);
+					async.parallel(steps, cb);
 
-        }, cb)
-      })
-    })
-  });
+				}, cb)
+			})
+		})
+	});
 }
 
 function rename(originalPath, newPath, supportGit, cb) {
-  if (supportGit) {
-    exec('git mv ' + originalPath + ' ' + newPath, function(err, stdout, stderr) {
-      if (err) return cb(err);
-      if (stderr) return cb(stderr);
-      cb();
-    })
-  } else {
-    fs.rename(originalPath, newPath, cb);
-  }
+	if (supportGit) {
+		exec('git mv ' + originalPath + ' ' + newPath, function(err, stdout, stderr) {
+			if (err) return cb(err);
+			if (stderr) return cb(stderr);
+			cb();
+		})
+	} else {
+		fs.rename(originalPath, newPath, cb);
+	}
 }
 
 function updateReferencesInMovedFile(originalFilePath, newFilePath, excludes, cb) {
-  fs.readFile(newFilePath, 'utf8', function(err, data) {
-    if (err) return cb(err);
+	fs.readFile(newFilePath, 'utf8', function(err, data) {
+		if (err) return cb(err);
 
-    var requires = getRequires(data);
+		var requires = getRequires(data);
 
-    if (requires) {
-      requires.forEach(function(oldRequire) {
-        var newRequire = generateNewRequire(oldRequire, originalFilePath, newFilePath, excludes);
-        if (newRequire)
-          data = data.replace(oldRequire, newRequire);
-      })
-    }
-    fs.writeFile(newFilePath, data, {encoding: 'utf8'}, cb);
-  });
+		if (requires) {
+			requires.forEach(function(oldRequire) {
+				var newRequire = generateNewRequire(oldRequire, originalFilePath, newFilePath, excludes);
+				if (newRequire)
+					data = data.replace(oldRequire, newRequire);
+			})
+		}
+		fs.writeFile(newFilePath, data, {encoding: 'utf8'}, cb);
+	});
 }
 
 function getRequires(fileData) {
-  var re = /require(\(|\s)('|")(\.\S+)('|")(\))?/g;
-  return fileData.match(re);
+	var re = /require(\(|\s)('|")(\.\S+)('|")(\))?/g;
+	return fileData.match(re);
 }
 
 function generateNewRequire(oldRequire, originalFilePath, newFilePath, excludes) {
-  var re = /require(\(|\s)('|")(\.\S+)('|")(\))?/,
-    groups = re.exec(oldRequire),
-    oldPath = groups[3],
-    oldAsbPath = path.join(path.dirname(originalFilePath), oldPath);
-    if (excludes && excludes.test(oldAsbPath)) return null;
-    newRelativePath = path.relative(path.dirname(newFilePath), oldAsbPath);
+	var re = /require(\(|\s)('|")(\.\S+)('|")(\))?/,
+		groups = re.exec(oldRequire),
+		oldPath = groups[3],
+		oldAsbPath = path.join(path.dirname(originalFilePath), oldPath);
+		if (excludes && excludes.test(oldAsbPath)) return null;
+		let newRelativePath = path.relative(path.dirname(newFilePath), oldAsbPath);
 
-  if (newRelativePath.indexOf('.') != 0 ) {
-    newRelativePath = './' + newRelativePath;
-  }
+	if (newRelativePath.indexOf('.') != 0 ) {
+		newRelativePath = './' + newRelativePath;
+	}
 
-  return oldRequire.replace(re, 'require$1$2' + newRelativePath + '$4$5');
+	return oldRequire.replace(re, 'require$1$2' + newRelativePath + '$4$5');
 }
 
 function updateReferencesToMovedFile(currentDir, originalFilePath, newFilePath, regExcludes, cb) {
-  walk(currentDir, regExcludes, function(err, files) {
-    if (err) return cb(err);
+	walk(currentDir, regExcludes, function(err, files) {
+		if (err) return cb(err);
 
-    function updateReferenceForFile(file, cb) {
-      var oldRelativePath = path.relative(path.dirname(file), originalFilePath).replace(/\.(js|coffee)$/, ''),
-        newRelativePath = path.relative(path.dirname(file), newFilePath).replace(/\.(js|coffee)$/, '');
-      if (oldRelativePath.indexOf(".") != 0 ) {
-        oldRelativePath = './' + oldRelativePath;
-      }
-      if (newRelativePath.indexOf(".") != 0 ) {
-        newRelativePath = './' + newRelativePath;
-      }
+		function updateReferenceForFile(file, cb) {
+			var oldRelativePath = path.relative(path.dirname(file), originalFilePath).replace(/\.(js|coffee)$/, ''),
+				newRelativePath = path.relative(path.dirname(file), newFilePath).replace(/\.(js|coffee)$/, '');
+			if (oldRelativePath.indexOf(".") != 0 ) {
+				oldRelativePath = './' + oldRelativePath;
+			}
+			if (newRelativePath.indexOf(".") != 0 ) {
+				newRelativePath = './' + newRelativePath;
+			}
 
-      var regex = exports.generateRequireRegex(oldRelativePath);
-      fs.readFile(file, 'utf8', function(err, data) {
-        if (err) return cb(err);
+			var regex = generateRequireRegex(oldRelativePath);
+			fs.readFile(file, 'utf8', function(err, data) {
+				if (err) return cb(err);
 
-        if (data.indexOf(regex)) {
-          var result = data.replace(regex, 'require$1$2' + newRelativePath + '$4$5');
-          return fs.writeFile(file, result, {encoding: 'utf8'}, cb);
-        } else {
-          return cb()
-        }
-      })
-    }
-    async.eachSeries(files, updateReferenceForFile, cb);
-  })
+				if (data.indexOf(regex.toString())) {
+					var result = data.replace(regex, 'require$1$2' + newRelativePath + '$4$5');
+					return fs.writeFile(file, result, {encoding: 'utf8'}, cb);
+				} else {
+					return cb()
+				}
+			})
+		}
+		async.eachSeries(files, updateReferenceForFile, cb);
+	})
 }
 
-exports.generateRequireRegex = function(filePath) {
-  return new RegExp("require(\\(|\\s)('|\")(" + filePath + ")('|\")(\\))?", "g");
+function generateRequireRegex(filePath) {
+	return new RegExp("require(\\(|\\s)('|\")(" + filePath + ")('|\")(\\))?", "g");
 }
+
+// module.exports = main
+module.exports = main
